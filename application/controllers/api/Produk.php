d37d7a67283ce6e5f02b15995c2b0f591888f599
unknown <ferdyfianaziizul@gmail.com>
2022-11-11 17:23:34 +0700

    commit ferdy

--- application/controllers/api/Produk.php
+++ application/controllers/api/Produk.php
@@ -27,30 +27,13 @@ class Produk extends REST_Controller
     {
         $param = $this->get();
         if (count($param) == 0) {
-            $this->db->order_by('tanggal_input', 'DESC');
-            $api = $this->db->get('produk')->result();
+            $this->db->order_by('updatedAt', 'DESC');
+            $api = $this->db->get('produk', 0, 0)->result();
         } else {
             $this->db->where($param);
             $api = $this->db->get('produk')->result();
         }
         $this->response(['message' => 'success', 'data' => $api, 'status' => 200], 200);
-    }
-    function index_post()
-    {
-
-        $data = $this->post();
-        $insert = $this->db->insert('produk', $data['produk']);
-        $insert_id = $this->db->insert_id();
-        $arr = array();
-        foreach ($data['ukuran'] as  $value) {
-            array_push($arr, ['ukuran_id' => $value['ukuran_id'], 'produk_id' => $insert_id]);
-        };
-        $insert_ukuran = $this->db->insert_batch('ukuran_produk', $arr);
-        if ($insert & $insert_ukuran) {
-            $this->response(['message' => "berhasil menambahkan produk", 'data' => $data['produk']], 200);
-        } else {
-            $this->response(['message' => "gagal menambahkan produk"], 502);
-        }
     }
     function index_put()
     {
@@ -66,11 +49,43 @@ class Produk extends REST_Controller
     function index_delete()
     {
         $id = $this->uri->segment("3");
-        $delete = $this->db->delete("motivasi", ['id' => $id]);
+        $this->db->where(['id' => $id]);
+        $produk = $this->db->get('produk')->result()[0];
+        unlink('.' . $produk->image);
+        $delete = $this->db->delete("produk", ['id' => $id]);
         if ($delete) {
-            $this->response(['message' => 'sucess delete motivasi'], 200);
+            $this->response(['message' => 'sucess delete produk'], 200);
         } else {
-            $this->response(['message' => 'error saat delete motivasi'], 502);
+            $this->response(['message' => 'error saat delete produk'], 502);
+        }
+    }
+
+    function index_post()
+    {
+        $config['upload_path'] = './images/';
+        $config['allowed_types'] = 'gif|jpg|jpeg|png|jfif|webp';
+        // $config['max_size']     = '100';
+        // $config['max_width'] = '1024';
+        // $config['max_height'] = '768';
+
+        $this->load->library('upload', $config);
+        $cek = $this->upload->do_upload('image');
+        if ($cek) {
+            $res = $this->upload->data();
+            $data = [
+                'name' => $this->post('name'),
+                'price' => $this->post('price'),
+                'description' => $this->post('description'),
+                'image' => '/images/' . $res['file_name']
+            ];
+            $insert = $this->db->insert('produk', $data);
+            if ($insert) {
+                $this->response(['message' => "berhasil menambahkan produk", 'data' => $data], 200);
+            } else {
+                $this->response(['message' => "gagal menambahkan produk"], 502);
+            }
+        } else {
+            $this->response(['message' => 'error upload'], 502);
         }
     }
 }
